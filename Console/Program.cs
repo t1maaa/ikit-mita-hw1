562f582cdbfd880bf7076196ea9a28241c90d7e5
t1maaa <t1maaa@mail.ru>
2017-05-04 22:15:11 +0700

    done

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgCat.Model;
 
 namespace ProgCat
 {
@@ -10,7 +11,105 @@ namespace ProgCat
     {
         static void Main(string[] args)
         {
-           
+            
+            Console.WriteLine("!Зоомагазин электронных котов!");
+
+            Console.WriteLine("Введите возраст кота:");
+            int age = SetAge();
+            
+            var cat = new Cat(age);
+
+            while (true)
+            {
+                CatMenu(cat);
+            }
+        }
+
+        public static int SetAge()
+        {
+            int age = -1;
+            do
+            {
+                try
+                {
+                    age = Convert.ToInt32(Console.ReadLine());
+                    if (age <= 0)
+                    {
+                        throw new Exception("Неверный возраст!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + "\nЕще раз: ");
+                    
+                }
+
+            } while (age <= 0);
+            
+            return age;
+        }
+
+        public static string SetName()
+        {
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        public static void CatMenu(Cat cat)
+        {
+            var menu = -1;
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine("1. Покормить кота");
+            Console.WriteLine("2. Наказать кота");
+
+            if ((String.IsNullOrEmpty(cat.Name)))
+            {
+                Console.WriteLine("3. Дать имя коту");
+                Console.WriteLine("\nИмя: -");
+            }
+            else
+            {
+                Console.WriteLine("\nИмя: {0}", cat.Name);
+            }
+            Console.WriteLine("Возраст: {0}", cat.Age);
+
+            switch (cat.Color())
+            {
+                case "Red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "Yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "Green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "White":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+
+            Console.WriteLine("Здоровье: {0}", cat.Health);
+
+            menu = (Console.ReadKey(true).KeyChar); 
+
+            switch ( menu)
+            {
+                case '1':
+                    cat.Feed();
+                    break;
+                case '2':
+                    cat.Punish();
+                    break;
+                case '3':
+                    Console.ResetColor();
+                    Console.WriteLine("\nВведите имя кота: ");
+                    cat.Name = SetName();
+                    break;
+
+                    
+            }
         }
     }
 }
---
+++ Model/Cat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgCat.Model
+{
+    class Cat
+    {
+        private int _health;
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public int Health => _health;
+
+        public int Age { get; }
+
+        public string Color()
+        {
+            if (this._health < 3)
+            {
+                return "Red";
+            }
+
+            else if (this._health > 5)
+            {
+                return "Green";
+            }
+
+            else if (this._health == 5)
+            {
+                return "Yellow";
+            }
+
+            else return "White";
+
+        }
+
+        public void Feed()
+        {
+            this._health++;
+        }
+
+        public void Punish()
+        {
+            this._health--;
+        }
+
+        public Cat(int age)
+        {
+            this.Age = age;
+            this._health = 6;
+        }
+    }
+}
